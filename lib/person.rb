35bfb13434169bd054368b0933fbf3bf46c37434
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-16 15:40:24 -0500

    commit tree 89dfa8b430899595ec203cfcc842beef996addad

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
